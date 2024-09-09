3eaf1e10cb7403ea518eac965f6db7d92d418d29
YOON JIEUN <yjeyje001020@naver.com>
2024-09-09 14:02:25 +0900

    Initial commit

---
+++ java.js
@@ -0,0 +1,30 @@
+var name = "zzzingnii";
+let age = 25;
+const  city = "Busan"
+
+console.log(typeof name); 
+console.log(typeof age);  
+console.log(typeof city);  
+
+let x = 20;
+let y = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+let firstName = "YOON";
+let lastName = "zzzingnii";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+if (age >= 18) {
+    console.log("성인입니다.");
+} else {
+    console.log("미성년자입니다.");
+}
+
+let status = (age >= 20) ? "성인" : "미성년자";
+console.log(status);
