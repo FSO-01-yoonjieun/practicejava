b3b2a1500722c680e7692086cbfe90aaffecaafe
YOON JIEUN <yjeyje001020@naver.com>
2024-09-09 18:59:28 +0900

    최종수정안

--- java.js
+++ java.js
@@ -175,28 +175,56 @@ console.log(add(10,5));
 
 let num = 7;
 if (num % 2 === 0) {
-    console.log("짝수입니다.");
+    console.log("짝수");
   } else {
-    console.log("홀수입니다.");
+    console.log("홀수");
   }
   
 // 13. 1부터 10까지의 숫자를 출력하는 for 반복문을 작성하세요.
 
+for (let z = 1; z < 11; z++){
+    console.log(z);
+}
+
 // 14. 1부터 10까지의 숫자를 출력하는 while 반복문을 작성하세요.
 
+let zz = 0;
+
+while (zz < 11) {
+    zz++;
+    console.log(zz);
+}
+
 // 15. 배열의 각 요소를 출력하는 forEach 반복문을 작성하세요.
-// let fruits = ['Apple', 'Banana', 'Cherry'];
+const fruits2 = ['Apple', 'Banana', 'Cherry'];
+
+fruits2.forEach((element) => {
+    console.log(element);
+});
+
 
 // 16. 배열의 각 요소에 2를 곱한 새로운 배열을 반환하는 코드를 작성하세요.
-// let numbers = [1, 2, 3, 4];
+
+const numbers = [1, 2, 3, 4];
+const mapped = numbers.map((number) => {
+    return number * 2;
+});
 
 // 17. 배열에서 짝수만 필터링하는 코드를 작성하세요.
-// let numbers = [1, 2, 3, 4, 5, 6];
+
+const numbers = [1, 2, 3, 4, 5, 6];
+numbers2.filter((number2) => {
+    return number2 % 2 === 0;
+});
+
 
 // 18. 문자열을 대문자로 변환하고 출력하는 코드를 작성하세요.
-// let str = 'hello world';
+const str = 'hello world'.toUpperCase();
+console.log(str);
 
 // 19. 현재 날짜와 시간을 출력하는 코드를 작성하세요.
+console.log(new Date());
 
 // 20. Math 객체를 사용하여 주어진 수의 제곱근을 구하는 코드를 작성하세요.
-// let num = 16;
+let num100 = 16;
+console.log(Math,squrt(num100));
